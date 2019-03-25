ba7fc8808adec0bf65172107d7f9e06d3e4f7245
rajkumar katanor <rkatanor@gmail.com>
2019-03-25 10:39:53 +0530

    added deployment server path

--- src/main/java/com/test/EdiApplication.java
+++ src/main/java/com/test/EdiApplication.java
@@ -1,30 +1,20 @@
 package com.test;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.test.amqproducer.AMQProducer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EdiApplication {
+public class EdiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdiApplication.class, args);
-		/*ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
-
-		while (true) {
-			AMQProducer producer = new AMQProducer();
-			try {
-				Thread.sleep(30000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			executor.execute(producer);
-		}*/
+	
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EdiApplication.class);
 	}
 
 }
